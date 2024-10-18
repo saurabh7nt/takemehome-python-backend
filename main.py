c85fcec2e1969c759495e187e36401152c0a986d
Saurabh Tajane <saurabhtajne07@gmail.com>
2024-10-18 14:48:23 +0530

    Python backend for takemehome project pushed

---
+++ main.py
@@ -0,0 +1,29 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+from pet_data import get_pet_data
+
+app = Flask(__name__)
+
+CORS(app)
+
+@app.route('/api/pets', methods=['GET'])
+def get_pets():
+    # Fetch the hardcoded data from pet_data.py
+    pets = get_pet_data()
+    return jsonify(pets)
+
+
+# Get details of a particular pet by name
+@app.route('/api/pets/<string:name>', methods=['GET'])
+def get_by_name(name):
+    # Fetch the hardcoded data from pet_data.py
+    pets = get_pet_data()
+    # Find the pet with the matching name
+    pet = next((pet for pet in pets if pet["name"].lower() == name.lower()), None)
+    if pet:
+        return jsonify(pet), 200  # Return the pet details if found
+    else:
+        return jsonify({"error": "Pet not found"}), 404  # Return 404 if not found
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ pet_data.py
@@ -0,0 +1,39 @@
+def get_pet_data():
+    return [
+        {
+            "name": "Kitty",
+            "age": "1 year",
+            "vaccinated": "Yes",
+            "size": "Medium",
+            "gender": "Female",
+            "sterilization": "Yes",
+            "imagePath": "/Images/Kitty01.webp"
+        },
+        {
+            "name": "Dollar",
+            "age": "1 year",
+            "vaccinated": "Yes",
+            "size": "Small",
+            "gender": "Male",
+            "sterilization": "Yes",
+            "imagePath": "/Images/dog02.webp"
+        },
+        {
+            "name": "Scooby",
+            "age": "1 year",
+            "vaccinated": "Yes",
+            "size": "Small",
+            "gender": "Male",
+            "sterilization": "Yes",
+            "imagePath": "/Images/dog11.webp"
+        },
+        {
+            "name": "Peter",
+            "age": "1 year",
+            "vaccinated": "Yes",
+            "size": "Medium",
+            "gender": "Male",
+            "sterilization": "Yes",
+            "imagePath": "/Images/cat02.webp"
+        }
+    ]
