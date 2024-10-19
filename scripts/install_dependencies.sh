d689c3d5f3f923b5626a17f24a6fa6e0e68c929b
Saurabh Tajane <saurabhtajne07@gmail.com>
2024-10-19 23:23:29 +0530

    installation file changed

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -6,21 +6,19 @@ set -e
 # Update the package repository
 sudo apt-get update -y
 
+# Install Python3, pip, and venv if they're not installed
+sudo apt-get install -y python3 python3-venv python3-pip
+
 # Create the directory for the application if it doesn't exist
 if [ ! -d "/home/ubuntu/pet-app" ]; then
-  mkdir -p /home/ubuntu/pet-app
-fi
-
-# Install Python3 and pip if they're not installed
-if ! command -v python3 &>/dev/null; then
-    sudo apt-get install -y python3 python3-venv python3-pip
+  sudo mkdir -p /home/ubuntu/pet-app
 fi
 
 # Create a virtual environment if it doesn't exist
 cd /home/ubuntu/pet-app
 
 if [ ! -d "venv" ]; then
-  python3 -m venv venv
+  sudo python3 -m venv venv
 fi
 
 # Activate the virtual environment
