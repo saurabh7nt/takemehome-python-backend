227506ee3ce8ef079891714b9df5e8529b399998
Saurabh Tajane <saurabhtajne07@gmail.com>
2024-10-24 11:04:07 +0530

    scripts changed

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -14,25 +14,4 @@ if [ ! -d "/var/www/python-pet-app" ]; then
   sudo mkdir -p /var/www/python-pet-app
 fi
 
-# Create a virtual environment if it doesn't exist
-cd /var/www/python-pet-app
 
-if [ ! -d "venv" ]; then
-  sudo python3 -m venv venv
-fi
-
-# Activate the virtual environment
-source venv/bin/activate
-
-# Debugging: List the contents of the directory
-echo "Current directory contents:"
-ls -la
-
-# Check if the requirements.txt file exists
-if [ -f "requirements.txt" ]; then
-  echo "Installing dependencies from requirements.txt"
-  pip install -r requirements.txt
-else
-  echo "requirements.txt not found"
-  exit 1
-fi
